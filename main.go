8e57e3a491d848e139b26e7e9fd50780eebd6284
Patrick D'appollonio <patrick@dappollonio.us>
2019-10-02 15:10:58 -0400

    Update to support stdin.

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type conf struct {
 	environmentFile  string
 	templateFile     string
 	rawTemplate      string
+	stdin            bool
 	customDelimiters string
 	strictMode       bool
 
@@ -44,6 +45,7 @@ func main() {
 	root.Flags().StringVarP(&configs.templateFile, "file", "f", "", "the template file to process (required)")
 	root.Flags().StringVarP(&configs.customDelimiters, "delimiter", "d", "", `delimiter (default "{{}}")`)
 	root.Flags().StringVarP(&configs.rawTemplate, "execute", "x", "", "a raw template to execute directly, without providing --file")
+	root.Flags().BoolVarP(&configs.stdin, "stdin", "i", false, "a stdin input to execute directly, without providing --file or --execute")
 	root.Flags().BoolVarP(&configs.strictMode, "strict", "s", false, "enables strict mode: if an environment variable in the file is defined but not set, it'll fail")
 
 	if err := root.Execute(); err != nil {
@@ -66,6 +68,14 @@ func command(w io.Writer, c conf) error {
 	var b *bytes.Buffer
 
 	if c.templateFile != "" {
+		if c.rawTemplate != "" {
+			return errors.New("defined both --file and --raw, only one must be used")
+		}
+
+		if c.stdin {
+			return errors.New("defined both --file and --stdin, only one must be used")
+		}
+
 		bt, err := loadFile(c.templateFile)
 		if err != nil {
 			return err
@@ -75,11 +85,36 @@ func command(w io.Writer, c conf) error {
 	}
 
 	if c.rawTemplate != "" {
+		if c.templateFile != "" {
+			return errors.New("defined both --raw and --file, only one must be used")
+		}
+
+		if c.stdin {
+			return errors.New("defined both --raw and --stdin, only one must be used")
+		}
+
 		b = bytes.NewBufferString(c.rawTemplate)
 	}
 
+	if c.stdin {
+		if c.templateFile != "" {
+			return errors.New("defined both --stdin and --file, only one must be used")
+		}
+
+		if c.rawTemplate != "" {
+			return errors.New("defined both --stdin and --raw, only one must be used")
+		}
+
+		bt, err := loadFile(os.Stdin.Name())
+		if err != nil {
+			return err
+		}
+
+		b = bt
+	}
+
 	if b == nil {
-		return errors.New("needs to specify either a template file (using --file) or a raw template (using --raw)")
+		return errors.New("needs to specify either a template file (using --file) or a raw template (using --raw or --stdin)")
 	}
 
 	if err := loadVirtualEnv(c.environmentFile); err != nil {
